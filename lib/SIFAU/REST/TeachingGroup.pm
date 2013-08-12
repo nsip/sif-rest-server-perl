7454f2175345532a138655cb6eae9c60f8bd36ad
Scott Penrose <scottp@dd.com.au>
2013-08-13 09:04:52 +1000

    Teaching group and readme notes

--- lib/SIFAU/REST/TeachingGroup.pm
+++ lib/SIFAU/REST/TeachingGroup.pm
@@ -8,12 +8,55 @@ our $VERSION = '0.1';
 prefix '/TeachingGroups';
 
 get '/' => sub {
-	autoGetCollection('TeachingGroups', 'TeachingGroup');
+	#autoGetCollection('TeachingGroups', 'TeachingGroup');
+	content_type 'application/xml';
+	return "<TeachingGroups>" . fake() . "</TeachingGroups>";
 };
 
 get '/:id' => sub {
-	autoGet('TeachingGroup', 'TeachingGroup');
+	#autoGet('TeachingGroup', 'TeachingGroup');
+	content_type 'application/xml';
+	return fake();
 };
+
+sub fake {
+	return q{<TeachingGroup RefId="fake01">
+  <SchoolYear>2007</SchoolYear>
+  <SchoolInfoRefId>DC56C532026B11E3A5325DE06940ABA3</SchoolInfoRefId>
+  <LocalId>X1</LocalId>
+  <ShortName>Shortish</ShortName>
+  <LongName>Longish</LongName>
+
+  <StudentList>
+    <TeachingGroupStudent>
+      <StudentPersonalRefId>164da5d9bcbf4cf8a058ba0b0efde9ba</StudentPersonalRefId>
+      <Name Type="LGL">
+        <FamilyName>DAVEY</FamilyName>
+        <GivenName>Tim</GivenName>
+      </Name>
+    </TeachingGroupStudent>
+    <TeachingGroupStudent>
+      <StudentPersonalRefId>DC7AA9C0026B11E3A5325DE06940ABA3</StudentPersonalRefId>
+      <Name Type="LGL">
+        <FamilyName>SAXTON</FamilyName>
+        <GivenName>Sam</GivenName>
+      </Name>
+    </TeachingGroupStudent>
+  </StudentList>
+
+  <TeacherList>
+    <TeachingGroupTeacher>
+      <StaffPersonalRefId>E9E07EDC026B11E3A5325DE06940ABA3</StaffPersonalRefId>
+      <Name Type="LGL">
+        <FamilyName>CORR</FamilyName>
+        <GivenName>Anne</GivenName>
+      </Name>
+      <Association>Class Teacher</Association>
+    </TeachingGroupTeacher>
+  </TeacherList>
+
+</TeachingGroup>};
+}		
 
 # CREATE Multiple
 post '/' => sub {
