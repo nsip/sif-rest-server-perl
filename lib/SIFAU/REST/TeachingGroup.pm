fa65a79a2eb89b447316412511c7836e31975bb1
Scott Penrose <scottp@dd.com.au>
2013-08-28 15:50:27 +1000

    Complex teaching group create and return

--- lib/SIFAU/REST/TeachingGroup.pm
+++ lib/SIFAU/REST/TeachingGroup.pm
@@ -3,60 +3,90 @@ use Dancer ':syntax';
 use Dancer::Plugin::REST;
 use Dancer::Plugin::Database;
 use SIFAU::REST::Plugin;
+use SIF::XML::Parse;
+use Data::Dumper;
+use XML::Simple;
 
 our $VERSION = '0.1';
 prefix '/TeachingGroups';
 
+sub xml2data {
+	my ($xml) = @_;
+	my $raw = XMLin($xml, 
+		ForceArray => [qw/TeachingGroupTeacher TeachingGroupStudent/],
+		ForceContent => 1,
+		KeepRoot => 1,
+	);
+	SIF::XML::Parse::normalise($raw);
+	return $raw;
+}
+
 get '/' => sub {
-	#autoGetCollection('TeachingGroups', 'TeachingGroup');
-	content_type 'application/xml';
-	return "<TeachingGroups>" . fake() . "</TeachingGroups>";
+	my $sth = database->prepare(q{SELECT RefId FROM TeachingGroup});
+	$sth->execute();
+	my @data;
+	while (my $r = $sth->fetchrow_hashref) {
+		push @data, entry($r->{RefId});
+	}
+	return returnXML({
+		status => 201,
+		template => 'TeachingGroups',
+		data => \@data,
+	});
 };
 
 get '/:id' => sub {
-	#autoGet('TeachingGroup', 'TeachingGroup');
-	content_type 'application/xml';
-	return fake();
+	return returnXML({
+		status => 201,
+		template => 'TeachingGroup',
+		entry => entry(params->{id}),
+		data => [],
+	});
 };
 
-sub fake {
-	return q{<TeachingGroup RefId="fake01">
-  <SchoolYear>2007</SchoolYear>
-  <SchoolInfoRefId>DC56C532026B11E3A5325DE06940ABA3</SchoolInfoRefId>
-  <LocalId>X1</LocalId>
-  <ShortName>Shortish</ShortName>
-  <LongName>Longish</LongName>
+sub entry {
+	my ($id) = @_;
 
-  <StudentList>
-    <TeachingGroupStudent>
-      <StudentPersonalRefId>164da5d9bcbf4cf8a058ba0b0efde9ba</StudentPersonalRefId>
-      <Name Type="LGL">
-        <FamilyName>DAVEY</FamilyName>
-        <GivenName>Tim</GivenName>
-      </Name>
-    </TeachingGroupStudent>
-    <TeachingGroupStudent>
-      <StudentPersonalRefId>DC7AA9C0026B11E3A5325DE06940ABA3</StudentPersonalRefId>
-      <Name Type="LGL">
-        <FamilyName>SAXTON</FamilyName>
-        <GivenName>Sam</GivenName>
-      </Name>
-    </TeachingGroupStudent>
-  </StudentList>
+	my $sth = database->prepare(q{SELECT * FROM TeachingGroup WHERE RefId = ?});
+	$sth->execute($id);
+	my $data = $sth->fetchrow_hashref();
+	$data->{students} = [];
+	$data->{teachers} = [];
 
-  <TeacherList>
-    <TeachingGroupTeacher>
-      <StaffPersonalRefId>E9E07EDC026B11E3A5325DE06940ABA3</StaffPersonalRefId>
-      <Name Type="LGL">
-        <FamilyName>CORR</FamilyName>
-        <GivenName>Anne</GivenName>
-      </Name>
-      <Association>Class Teacher</Association>
-    </TeachingGroupTeacher>
-  </TeacherList>
+	# Students
+	$sth = database->prepare(q{
+		SELECT
+			StudentPersonal.RefId, 
+			StudentPersonal.GivenName, StudentPersonal.FamilyName
+		FROM
+			StudentPersonal, TeachingGroup_Student 
+		WHERE
+			StudentPersonal.RefId = TeachingGroup_Student.StudentPersonal_RefId
+			AND TeachingGroup_Student.TeachingGroup_RefId = ?
+	});
+	$sth->execute($id);
+	while ( my $r  = $sth->fetchrow_hashref()) {
+		push @{$data->{students}}, { %$r };
+	}
 
-</TeachingGroup>};
-}		
+	# Teachers
+	$sth = database->prepare(q{
+		SELECT
+			StaffPersonal.RefId, 
+			StaffPersonal.GivenName, StaffPersonal.FamilyName
+		FROM
+			StaffPersonal, TeachingGroup_Teacher 
+		WHERE
+			StaffPersonal.RefId = TeachingGroup_Teacher.StaffPersonal_RefId
+			AND TeachingGroup_Teacher.TeachingGroup_RefId = ?
+	});
+	$sth->execute($id);
+	while ( my $r  = $sth->fetchrow_hashref()) {
+		push @{$data->{teachers}}, { %$r };
+	}
+
+	return $data;
+}
 
 # CREATE Multiple
 post '/' => sub {
@@ -65,7 +95,62 @@ post '/' => sub {
 
 # CREATE Single
 post '/TeachingGroup' => sub {
-	autoCreate('TeachingGroup', 'TeachingGroup');
+	my $data = xml2data(request->body);
+	info(Dumper($data));
+
+	# Fields: ShortName, LongName, SchoolInfoRefId, LocalId
+	# Groups: 
+	# 	TeacherList/TeachingGroupTeacher -> StaffPersonalRefId/content
+	# 	StudentList/TeachingGroupStudent -> StudentPersonalRefId/content
+
+	my $refid = createRefId();
+	my @bind = ();
+	push @bind, $refid;
+	push @bind, eval { $data->{TeachingGroup}{ShortName}{content} } // "";
+	push @bind, eval { $data->{TeachingGroup}{LongName}{content} } // "";
+	push @bind, eval { $data->{TeachingGroup}{LocalId}{content} } // "";
+	push @bind, eval { $data->{TeachingGroup}{SchoolYear}{content} } // "";
+	push @bind, eval { $data->{TeachingGroup}{SchoolInfoRefId}{content} } // "";
+	
+	info(join(",", @bind));
+	my $sth = database->prepare(q{
+		INSERT INTO TeachingGroup
+			(RefId, ShortName, LongName, LocalId, SchoolYear, SchoolInfo_RefId)
+		VALUES
+			(?, ?, ?, ?, ?, ?)
+	});
+	$sth->execute( @bind );
+
+	my $sth_teacher = database->prepare(q{
+		INSERT INTO TeachingGroup_Teacher
+			(TeachingGroup_RefId, StaffPersonal_RefId)
+		VALUES
+			(?, ?)
+	});
+	foreach my $t ( eval { @{$data->{TeachingGroup}{TeacherList}{TeachingGroupTeacher}} } ) {
+		$sth_teacher->execute($refid, $t->{StaffPersonalRefId}{content});
+	}
+
+	my $sth_teacher = database->prepare(q{
+		INSERT INTO TeachingGroup_Student
+			(TeachingGroup_RefId, StudentPersonal_RefId)
+		VALUES
+			(?, ?)
+	});
+	foreach my $t ( eval { @{$data->{TeachingGroup}{StudentList}{TeachingGroupStudent}} } ) {
+		$sth_teacher->execute($refid, $t->{StudentPersonalRefId}{content});
+	}
+
+	# TODO addQueue
+	#addQueue({
+	#	name => 'StudentPersonals',
+	#	action => 'CREATE',
+	#	xml => '<example>end</example>',
+	#});
+	
+	database->commit();
+
+	return forward '/TeachingGroups/' . $refid, { id => $refid }, {method => 'GET'};
 };
 
 # Update set
@@ -75,7 +160,7 @@ put '/' => sub {
 
 # Update one
 put '/:id' => sub {
-	autoUpdate('TeachingGroup', 'TeachingGroup', params->{id});
+	die "Not implemented";
 };
 
 # Delete Set
@@ -85,7 +170,7 @@ del '/' => sub {
 
 # Delete Single
 del '/:id' => sub {
-	autoDelete('TeachingGroup', 'TeachingGroup', params->{id});
+	die "Not implemented";
 };
 
 true;
